d22dea23e4068e4dc6eaf64bc16b07734b1dfc5d
tanjera <ibi.keller@gmail.com>
2016-07-20 23:03:32 -0400

    testing generic mining op

---
+++ mining.op.js
@@ -0,0 +1,95 @@
+var MiningOp = {
+
+    run: function(creep, rmDeliver, rmHarvest, srcHarvest) {
+        
+        // Burrower?
+        if (creep.carryCapacity == 0) {
+            creep.memory.state = 'getenergy';
+        }
+        
+        // Manage machine states for carriers!
+        else if (creep.memory.state == 'working' && creep.carry[RESOURCE_ENERGY] == 0) {
+            creep.memory.state = 'getenergy';
+        }
+        else if (creep.memory.state == 'getenergy' && creep.carry[RESOURCE_ENERGY] == creep.carryCapacity) {
+            creep.memory.state = 'working';
+        }
+        else if (creep.memory.state != 'getenergy' && creep.memory.state != 'working') {
+            creep.memory.state = 'working';
+        }
+        
+	    if(creep.memory.state == 'getenergy') {
+	        if (creep.room != rmHarvest) {
+    	        var route = Game.map.findRoute(creep.room, rmHarvest);
+                if (route.length > 0) {
+                    var exit = creep.pos.findClosestByRange(route[0].exit);
+                    creep.moveTo(exit);
+                }
+	        }
+	        else if (creep.room == rmHarvest) {
+    	        var source;
+    	        
+    	        // Carriers, try to pick up off the ground first
+                if (creep.carryCapacity > 0) {
+                    source = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY);
+                    
+                    if (source != null)  {
+                        if (creep.pickup(source) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(source);
+                        }
+                    } else {
+                        source = srcHarvest;
+                        
+                        if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(source);
+                        }
+                    }
+                } else { // Burrowers, straight to the source
+                    
+                    source = srcHarvest;
+                    
+                    if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(source);
+                    }
+                }
+	        }
+        }
+        
+        if (creep.memory.state == 'working') { 
+            
+            if (creep.room == rmDeliver) {
+	            var target;
+                // Deliver to Spawns and extensions as priority
+                target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                        filter: (structure) => {
+                            return (structure.structureType == STRUCTURE_SPAWN && structure.energy < structure.energyCapacity)
+                                || (structure.structureType == STRUCTURE_EXTENSION && structure.energy < structure.energyCapacity)
+                                || (structure.structureType == STRUCTURE_TOWER && structure.energy < structure.energyCapacity / 2);
+                        }
+                });
+                // And to extensions/containers otherwise
+                if (target == null) {
+                    target = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                            filter: (structure) => {
+                                return (structure.structureType == STRUCTURE_CONTAINER && _.sum(structure.store) < structure.storeCapacity)
+                                || (structure.structureType == STRUCTURE_STORAGE && _.sum(structure.store) < structure.storeCapacity);
+                            }
+                    })
+                };
+                
+                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target);
+	            }
+	        }
+	        else if (creep.room != rmDeliver) {
+	            var route = Game.map.findRoute(creep.room, rmDeliver);
+                if (route.length > 0) {
+                    var exit = creep.pos.findClosestByRange(route[0].exit);
+                    creep.moveTo(exit);
+                }
+	        }
+        }
+	}
+};
+
+module.exports = w16s41Miner;
