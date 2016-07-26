7a6c76e730d4adad99f1a439e766b863a689ca57
tanjera <ibi.keller@gmail.com>
2016-07-26 11:36:26 -0400

    pop adjust

--- main.js
+++ main.js
@@ -29,7 +29,7 @@ module.exports.loop = function () {
     /* Colonies and in-colony mining operations */
     siteMining.run(Game.spawns.Spawn1, 'W18S43', 'W18S43', 1, 2, 0);    // W18S43 colony #1 mining
     siteMining.run(Game.spawns.Spawn2, 'W16S43', 'W16S43', 1, 2, 0);    // W16S43 colony #2 mining
-    siteColony.run(Game.spawns.Spawn1, 'W18S43', 1, 2, 0);     // W16S43 colony #1
+    siteColony.run(Game.spawns.Spawn1, 'W18S43', 1, 3, 0);     // W16S43 colony #1
     siteColony.run(Game.spawns.Spawn2, 'W16S43', 1, 2, 0);     // W16S43 colony #2
 
     /* All other mining operations */
