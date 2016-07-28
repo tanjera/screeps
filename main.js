53ca0457e15393a42a47b22959ba83f8e00b5d8d
Ibi Keller <ibi.keller@gmail.com>
2016-07-28 16:32:51 -0400

    implementing utilColony.Spawn(room)

--- main.js
+++ main.js
@@ -14,24 +14,26 @@ module.exports.loop = function () {
 
     /* TO DO:
           mineral extracting implemented- needs debugging?
+          implement spawning from multiple spawns in one room
+                make function utilColony.spawn(room)
     */     
 
 
-    // siteColony.run(spawn, rmColony, popRepairer, popWorker, popSoldier)
-    // siteMining.run(spawn, rmDeliver, rmHarvest, popBurrower, popCarrier, popMiner, popReserver, popMineralist)
+    // siteColony.run(rmColony, popRepairer, popWorker, popSoldier)
+    // siteMining.run(rmDeliver, rmHarvest, popBurrower, popCarrier, popMiner, popReserver, popMineralist)
 
     /* Colonies and in-colony mining operations */
-    siteMining.run(Game.spawns.Spawn1, 'W18S43', 'W18S43', 1, 2, 0, 0, 0);    // W18S43 colony #1 mining
-    siteMining.run(Game.spawns.Spawn2, 'W16S43', 'W16S43', 1, 1, 0, 0, 0);    // W16S43 colony #2 mining
-    siteColony.run(Game.spawns.Spawn1, 'W18S43', 1, 2, 0);     // W16S43 colony #1
-    siteColony.run(Game.spawns.Spawn2, 'W16S43', 1, 1, 0);     // W16S43 colony #2
+    siteMining.run('W18S43', 'W18S43', 1, 2, 0, 0, 1);    // W18S43 colony #1 mining
+    siteMining.run('W16S43', 'W16S43', 1, 1, 0, 0, 1);    // W16S43 colony #2 mining
+    siteColony.run('W18S43', 0, 3, 0);     // W16S43 colony #1
+    siteColony.run('W16S43', 1, 1, 0);     // W16S43 colony #2
 
     /* All other mining operations */
-    siteMining.run(Game.spawns.Spawn1, 'W18S43', 'W17S43', 1, 3, 0, 1, 0);    // W17S43 mining operation (from Colony #1, W18S43)
-    siteMining.run(Game.spawns.Spawn1, 'W18S43', 'W19S43', 1, 2, 0, 1, 0);    // W19S43 mining operation (from Colony #1, W18S43)
+    siteMining.run('W18S43', 'W17S43', 1, 3, 0, 1, 0);    // W17S43 mining operation (from Colony #1, W18S43)
+    siteMining.run('W18S43', 'W19S43', 1, 2, 0, 1, 0);    // W19S43 mining operation (from Colony #1, W18S43)
     
-    siteMining.run(Game.spawns.Spawn2, 'W16S43', 'W16S42', 1, 3, 0, 1, 0);    // W16S42 mining operation (from Colony #2, W16S43)
-    //siteMining.run(Game.spawns.Spawn2, 'W16S43', 'W17S42', 1, 2, 0, 0, 0);    // W17S42 mining operation (from Colony #2, W16S43)
-    siteMining.run(Game.spawns.Spawn2, 'W16S43', 'W16S41', 1, 2, 0, 1, 0);    // W16S41 mining operation (from Colony #2, W16S43)
+    siteMining.run('W16S43', 'W16S42', 1, 3, 0, 1, 0);    // W16S42 mining operation (from Colony #2, W16S43)
+    //siteMining.run('W16S43', 'W17S42', 1, 2, 0, 0, 0);    // W17S42 mining operation (from Colony #2, W16S43)
+    siteMining.run('W16S43', 'W16S41', 1, 2, 0, 1, 0);    // W16S41 mining operation (from Colony #2, W16S43)
     
 }
