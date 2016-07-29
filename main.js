f7ab4a8daa1a88aad45de61b404cbb5d9bfda05b
tanjera <ibi.keller@gmail.com>
2016-07-29 13:47:07 -0400

    misc changes, new colony

--- main.js
+++ main.js
@@ -13,6 +13,12 @@ module.exports.loop = function () {
 
 
     /* TO DO:
+        start spawning new creeps to replace ones with little time left 
+            listWorker -= listWorker(filter {with < 200 ticks})
+            (pitfall: make sure it doesn't trigger 3 spawns to spawn the same replacement!!!)
+
+        for distant spawning: maybe sort spawns for availability and attempt closest spawn first????
+            otherwise everything will spawn from main colony???
     */     
 
 
@@ -20,14 +26,16 @@ module.exports.loop = function () {
     // siteMining.run(rmDeliver, rmHarvest, popBurrower, popCarrier, popMiner, popReserver, popMineralist)
 
     /* Colonies and in-colony mining operations */
-    siteMining.run('W18S43', 'W18S43', 1, 2, 0, 0, 1);    // W18S43 colony #1 mining
+    siteMining.run('W18S43', 'W18S43', 1, 2, 0, 0, 0);    // W18S43 colony #1 mining
     siteMining.run('W16S43', 'W16S43', 1, 1, 0, 0, 1);    // W16S43 colony #2 mining
-    siteColony.run('W18S43', 0, 3, 0);     // W16S43 colony #1
-    siteColony.run('W16S43', 1, 1, 0);     // W16S43 colony #2
+    siteMining.run('W19S42', 'W19S42', 1, 1, 0, 0, 0);    // W19S42 colony #2 mining
+    siteColony.run('W18S43', 1, 2, 0);     // W16S43 colony #1
+    siteColony.run('W16S43', 1, 2, 0);     // W16S43 colony #2
+    siteColony.run('W19S42', 0, 1, 0);     // W19S42 colony #3
 
     /* All other mining operations */
-    siteMining.run('W18S43', 'W17S43', 1, 3, 0, 1, 0);    // W17S43 mining operation (from Colony #1, W18S43)
-    siteMining.run('W18S43', 'W19S43', 1, 2, 0, 1, 0);    // W19S43 mining operation (from Colony #1, W18S43)
+    siteMining.run('W18S43', 'W17S43', 1, 2, 0, 1, 0);    // W17S43 mining operation (from Colony #1, W18S43)
+    siteMining.run('W18S43', 'W19S43', 1, 1, 0, 1, 0);    // W19S43 mining operation (from Colony #1, W18S43)
     siteMining.run('W18S43', 'W17S42', 1, 2, 0, 1, 0);    // W17S42 mining operation (from Colony #1, W18S43)
     
     siteMining.run('W16S43', 'W16S42', 1, 3, 0, 1, 0);    // W16S42 mining operation (from Colony #2, W16S43)
--- role.worker.js
+++ role.worker.js
@@ -1,117 +1,124 @@
+var utilCreep = require('util.creep');
+
 var RoleWorker = {
 
     run: function(creep) {
 
-        var _ticksReusePath = 10;
+        if (creep.memory.room != null && creep.room.name != creep.memory.room) {
+            utilCreep.moveToRoom(creep, creep.memory.room);
+        }
+        else {
+            var _ticksReusePath = 10;
 
-        // Manage machine states!
-        if (creep.memory.state == 'working' && creep.carry[RESOURCE_ENERGY] == 0) {
-            creep.memory.state = 'getenergy';
-        }
-        else if (creep.memory.state == 'getenergy' && creep.carry[RESOURCE_ENERGY] == creep.carryCapacity) {
-            creep.memory.state = 'working';
-        }
-        else if (creep.memory.state != 'getenergy' && creep.memory.state != 'working') {
-            creep.memory.state = 'working';
-        }
+            // Manage machine states!
+            if (creep.memory.state == 'working' && creep.carry[RESOURCE_ENERGY] == 0) {
+                creep.memory.state = 'getenergy';
+            }
+            else if (creep.memory.state == 'getenergy' && creep.carry[RESOURCE_ENERGY] == creep.carryCapacity) {
+                creep.memory.state = 'working';
+            }
+            else if (creep.memory.state != 'getenergy' && creep.memory.state != 'working') {
+                creep.memory.state = 'working';
+            }
 
-        // Out of energy? Find more...
-	    if(creep.memory.state == 'getenergy') {
-            var source;
-            
-            // Try to pick up off the ground first, if there aren't any carriers around
-            if (creep.room.find(FIND_MY_CREEPS, { filter: function(c) 
-                    { return c.memory.role == 'carrier' && c.memory.room == creep.room.name && c.memory.state == 'getenergy'; }}).length == 0) {
+            // Out of energy? Find more...
+            if(creep.memory.state == 'getenergy') {
+                var source;
                 
-                source = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY, { filter: (s) => { return s.amount >= creep.carryCapacity / 2; }});
-                if (source != null && creep.pickup(source) == ERR_NOT_IN_RANGE) {                
+                // Try to pick up off the ground first, if there aren't any carriers around
+                if (creep.room.find(FIND_MY_CREEPS, { filter: function(c) 
+                        { return c.memory.role == 'carrier' && c.memory.room == creep.room.name && c.memory.state == 'getenergy'; }}).length == 0) {
+                    
+                    source = creep.pos.findClosestByRange(FIND_DROPPED_ENERGY, { filter: (s) => { return s.amount >= creep.carryCapacity / 2; }});
+                    if (source != null && creep.pickup(source) == ERR_NOT_IN_RANGE) {                
+                        creep.moveTo(source, {reusePath: _ticksReusePath});
+                        return;
+                    }
+                }
+                
+                // Try to pull energy from storage containers...
+                source = creep.pos.findClosestByRange(FIND_STRUCTURES, {
+                            filter: (structure) => {
+                                return (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0)
+                                        || (structure.structureType == STRUCTURE_STORAGE && structure.store[RESOURCE_ENERGY] > 0);
+                            }});
+                
+                if (source != null) {
+                    if (source.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(source, {reusePath: _ticksReusePath});
+                        return;
+                    }
+                }
+                
+                // But if there are none... then harvest from a source
+                source = creep.pos.findClosestByPath(FIND_SOURCES);
+                if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(source, {reusePath: _ticksReusePath});
                     return;
-                }
+                } 
             }
             
-            // Try to pull energy from storage containers...
-            source = creep.pos.findClosestByRange(FIND_STRUCTURES, {
-                        filter: (structure) => {
-                            return (structure.structureType == STRUCTURE_CONTAINER && structure.store[RESOURCE_ENERGY] > 0)
-                                    || (structure.structureType == STRUCTURE_STORAGE && structure.store[RESOURCE_ENERGY] > 0);
-                        }});
-            
-            if (source != null) {
-                if (source.transfer(creep, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(source, {reusePath: _ticksReusePath});
-                    return;
+
+            else if (creep.memory.state == 'working') {
+            // Order of functions: upgrade critical downgrade timer, repair, build, then upgrade extra
+
+                if (creep.room.controller != null && creep.room.controller.ticksToDowngrade < 5000) {
+                    var r = creep.upgradeController(creep.room.controller); 
+                    if (r == OK) return;
+                    else if (r == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(creep.room.controller, {reusePath: _ticksReusePath});
+                        return;
+                    }
                 }
-            }
-            
-            // But if there are none... then harvest from a source
-            source = creep.pos.findClosestByPath(FIND_SOURCES);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {reusePath: _ticksReusePath});
-                return;
-            } 
-	    }
-	    
 
-	    else if (creep.memory.state == 'working') {
-        // Order of functions: upgrade critical downgrade timer, repair, build, then upgrade extra
-
-            if (creep.room.controller != null && creep.room.controller.ticksToDowngrade < 5000) {
-                var r = creep.upgradeController(creep.room.controller); 
-                if (r == OK) return;
-                else if (r == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(creep.room.controller, {reusePath: _ticksReusePath});
-                    return;
-                }
-            }
-
-            var structure;
-            // Repair *critical* structures
-            var uc = require('util.colony');
-            var structure = uc.findByNeed_RepairCritical(creep.room);
-            if (structure != null) {
-                var r = creep.repair(structure); 
-                if (r == OK) return;
-                else if(r == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(structure, {reusePath: _ticksReusePath});
-                    return;
-                }
-            }
-
-            // Build construction sites
-            structure = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
-            if (structure != null) {
-                var r = creep.build(structure);
-                if (r == OK) return;
-                else if (r == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(structure, {reusePath: _ticksReusePath});
-                    return;
-                }
-            }
-
-            // Repair *maintenance* for subrole repairers
-            if (creep.memory.subrole == 'repairer') {
+                var structure;
+                // Repair *critical* structures
                 var uc = require('util.colony');
-                structure = uc.findByRange_RepairMaintenance(creep);
+                var structure = uc.findByNeed_RepairCritical(creep.room);
                 if (structure != null) {
-                    var r = creep.repair(structure);
+                    var r = creep.repair(structure); 
+                    if (r == OK) return;
+                    else if(r == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(structure, {reusePath: _ticksReusePath});
+                        return;
+                    }
+                }
+
+                // Build construction sites
+                structure = creep.pos.findClosestByRange(FIND_CONSTRUCTION_SITES);
+                if (structure != null) {
+                    var r = creep.build(structure);
                     if (r == OK) return;
                     else if (r == ERR_NOT_IN_RANGE) {
                         creep.moveTo(structure, {reusePath: _ticksReusePath});
                         return;
                     }
                 }
-            }
 
-            // Or upgrade the controller
-            var r = creep.upgradeController(creep.room.controller);
-            if (r == OK) return;
-            else if (r == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller, {reusePath: _ticksReusePath});
-                return;
+                // Repair *maintenance* for subrole repairers
+                if (creep.memory.subrole == 'repairer') {
+                    var uc = require('util.colony');
+                    structure = uc.findByRange_RepairMaintenance(creep);
+                    if (structure != null) {
+                        var r = creep.repair(structure);
+                        if (r == OK) return;
+                        else if (r == ERR_NOT_IN_RANGE) {
+                            creep.moveTo(structure, {reusePath: _ticksReusePath});
+                            return;
+                        }
+                    }
+                }
+
+                // Or upgrade the controller
+                var r = creep.upgradeController(creep.room.controller);
+                if (r == OK) return;
+                else if (r == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(creep.room.controller, {reusePath: _ticksReusePath});
+                    return;
+                }
             }
-	    }
-	}
+        }
+    }
 };
 
 module.exports = RoleWorker;
