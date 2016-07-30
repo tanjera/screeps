bbeeda0eee0b0feb0577f1abb67b920bb94d7a50
tanjera <ibi.keller@gmail.com>
2016-07-29 20:07:14 -0400

    order of operations

--- main.js
+++ main.js
@@ -15,18 +15,18 @@ module.exports.loop = function () {
     /* TO DO:
     */     
 
-
+    // Order of operations: what's run last actually processes (e.g. spawning)
     // siteColony.run(rmColony, popRepairer, popWorker, popSoldier)
     // siteMining.run(rmDeliver, rmHarvest, popBurrower, popCarrier, popMiner, popReserver, popMineralist)
 
     /* Colonies and in-colony mining operations */
+    siteColony.run('W18S43', 1, 2, 0);     // W16S43 colony #1
+    siteColony.run('W16S43', 1, 2, 0);     // W16S43 colony #2
+    siteColony.run('W19S42', 2, 6, 0);     // W19S42 colony #3    
     siteMining.run('W18S43', 'W18S43', 1, 2, 0, 0, 0);    // W18S43 colony #1 mining
     siteMining.run('W16S43', 'W16S43', 1, 1, 0, 0, 1);    // W16S43 colony #2 mining
     siteMining.run('W19S42', 'W19S42', 2, 2, 0, 0, 0);    // W19S42 colony #3 mining
-    siteColony.run('W18S43', 1, 2, 0);     // W16S43 colony #1
-    siteColony.run('W16S43', 1, 2, 0);     // W16S43 colony #2
-    siteColony.run('W19S42', 2, 6, 0);     // W19S42 colony #3
-
+    
     /* All other mining operations */
     siteMining.run('W18S43', 'W17S43', 1, 2, 0, 1, 0);    // W17S43 mining operation (from Colony #1, W18S43)
     siteMining.run('W18S43', 'W19S43', 1, 1, 0, 1, 0);    // W19S43 mining operation (from Colony #1, W18S43)
