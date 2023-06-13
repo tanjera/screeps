e1eda5a7adbc3eb8bc1180068891b284a6885d30
AZcoigreach <azcoigreach@gmail.com>
2023-06-13 08:54:42 -0700

    debug visuals

--- main.js
+++ main.js
@@ -40,7 +40,7 @@
 
 {	/* Constants */
 
-// Defensive alert states
+	// Defensive alert states
 	NONE = 10;
 	LOW = 11;
 	MEDIUM = 12;
@@ -1562,7 +1562,7 @@ RoomPosition.prototype.getOpenTile_Path = function getOpenTile_Path(range, creep
 			}
 		}
 	}
-
+sec02aworker
 	return null;
 };
 
@@ -7857,6 +7857,15 @@ let Stats_Visual = {
 			if (_.get(Memory, ["rooms", r, "camp"]) != null)
 				new RoomVisual(r).circle(_.get(Memory, ["rooms", r, "camp"]),
 					{ fill: "orange", stroke: "pink", radius: 0.3, opacity: 0.25 });
+		});
+		// Show all Screeps paths as a blue line
+		_.each(_.filter(Game.creeps, c => { return c.memory._move != null }), c => {
+			let path = c.memory._move.path;
+			for (let i = 0; i < path.length; i++) {
+				new RoomVisual(c.room.name).circle(path[i].x, path[i].y, { fill: "blue", radius: 0.15, opacity: 0.25 });
+				if (i > 0)
+					new RoomVisual(c.room.name).line(path[i - 1].x, path[i - 1].y, path[i].x, path[i].y, { color: "blue", opacity: 0.25 });
+			}
 		});
 	},
 
